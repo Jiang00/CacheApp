7013d520f9301f3e5bb3a3b60d118d41e72efbce
Shan <shliang@gmail.com>
2015-12-02 15:06:16 -0500

    Update

--- src/CacheSync/ChatClient.java
+++ src/CacheSync/ChatClient.java
@@ -40,7 +40,9 @@ public class ChatClient implements Runnable {
   
  
        try {
+                       
             socket = new Socket(serverName, serverPort);
+            socket.setSoTimeout(5*1000);
             System.out.println("Connected: " + socket);
             isConnected = true;
             SwingUtilities.invokeLater(new Runnable() {
